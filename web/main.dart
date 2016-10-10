6733884573eaef6b5e83d3698cd2acab5d05a357
kyorohiro <kyorohiro@gmail.com>
2016-10-11 06:01:58 +0900

    sleeping now

--- web/main.dart
+++ web/main.dart
@@ -8,9 +8,12 @@ void main() {
 
 class ContentBuilder {
   String navigatorId = "fire-navigation";
+  String navigatorRightId = "fire-navigation-right";
+  String navigatorLeftId = "fire-navigation-left";
   String contentId = "fire-content";
   String footerId = "fire-footer";
 
+  List<String> tab = ["xxx","yyy"];
   bake(html.Element rootElm) {
     rootElm.appendHtml([
       """<div id=${navigatorId} class="${navigatorId}"> </div>""",
@@ -18,6 +21,15 @@ class ContentBuilder {
       """<div id=${footerId} class="${footerId}"> </div>""",
     ].join("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
     //
-
+    var navigator = rootElm.querySelector("#${navigatorId}");
+    navigator.appendHtml([
+      """<div id=${navigatorRightId} class="${navigatorRightId}"> </div>""",
+      """<div id=${navigatorLeftId} class="${navigatorLeftId}"> </div>""",
+    ], treeSanitizer: html.NodeTreeSanitizer.trusted);
+    //
+    var navigatorRight = rootElm.querySelector("#${navigatorRightId}");
+    for(String item in tab) {
+      navigatorRight.appendHtml("<div> ${item} </div>");
+    }
   }
 }
