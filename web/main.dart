50ab3de4073ba962743732ca4d3880c43e4dd00c
kyorohiro <kyorohiro@gmail.com>
2016-10-12 03:10:04 +0900

    ..

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@ import 'dart:html' as html;
 
 void main() {
   print("hello client");
-  ContentBuilder c = new ContentBuilder();
+  ContentBuilder c = new ContentBuilder()..addItem("ME", "#/Me")..addItem("Home", "#/Home");
   c.bake(html.document.body);
 }
 
@@ -12,24 +12,60 @@ class ContentBuilder {
   String navigatorLeftId = "fire-navigation-left";
   String contentId = "fire-content";
   String footerId = "fire-footer";
+  String navigatorItemId = "fire-naviitem";
 
-  List<String> tab = ["xxx","yyy"];
+  List<String> tabList = [];
+  List<String> urlList = [];
+  Map<String, html.Element> elm = {};
+
+  ContentBuilder() {
+    html.window.onHashChange.listen((html.Event ev) {
+      onHashChange();
+    });
+  }
+
+  void addItem(String label, String url) {
+    tabList.add(label);
+    urlList.add(url);
+  }
+
+  void onHashChange() {
+    //::selectio
+    //  var e = elm[html.window.location.hash];
+  }
+
   bake(html.Element rootElm) {
-    rootElm.appendHtml([
-      """<div id=${navigatorId} class="${navigatorId}"> </div>""",
-      """<div id=${contentId} class="${contentId}"> </div>""",
-      """<div id=${footerId} class="${footerId}"> </div>""",
-    ].join("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
+    rootElm.appendHtml(["""<div id=${navigatorId} class="${navigatorId}"> </div>""", """<div id=${contentId} class="${contentId}"> </div>""", """<div id=${footerId} class="${footerId}"> </div>""",].join("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
     //
     var navigator = rootElm.querySelector("#${navigatorId}");
-    navigator.appendHtml([
-      """<div id=${navigatorRightId} class="${navigatorRightId}"> </div>""",
-      """<div id=${navigatorLeftId} class="${navigatorLeftId}"> </div>""",
-    ], treeSanitizer: html.NodeTreeSanitizer.trusted);
+    navigator.appendHtml(
+        [
+          """<div id=${navigatorLeftId} class="${navigatorLeftId}"> </div>""",
+          """<div id=${navigatorRightId} class="${navigatorRightId}"> </div>""", //
+        ].join("\r\n"),
+        treeSanitizer: html.NodeTreeSanitizer.trusted);
+    //
     //
     var navigatorRight = rootElm.querySelector("#${navigatorRightId}");
-    for(String item in tab) {
-      navigatorRight.appendHtml("<div> ${item} </div>");
+    navigatorRight.appendHtml("""<div style="right:100;" class="${navigatorItemId}">(- -)</div>""", treeSanitizer: html.NodeTreeSanitizer.trusted);
+
+    //
+    var navigatorLeft = rootElm.querySelector("#${navigatorLeftId}");
+    elm.clear();
+    for (int i = 0; i < tabList.length; i++) {
+      var item = new html.Element.html("""<a href="${urlList[i]}" id="${navigatorItemId}" class="${navigatorItemId}"> ${tabList[i]} </a>""", treeSanitizer: html.NodeTreeSanitizer.trusted);
+      navigatorLeft.children.add(item);
+      elm[urlList[i]] = item;
+      item.onClick.listen((e) {
+        for (var ee in elm.values) {
+          ee.classes.clear();
+          if (item != ee) {
+            ee.classes.add("${navigatorItemId}");
+          } else {
+            ee.classes.add("${navigatorItemId}-checked");
+          }
+        }
+      });
     }
   }
 }
