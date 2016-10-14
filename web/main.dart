92ede2e6fa76e7413826f303795e4708f13893d0
kyorohiro <kyorohiro@gmail.com>
2016-10-15 06:33:58 +0900

    add twitter login

--- web/main.dart
+++ web/main.dart
@@ -1,58 +1,79 @@
 import 'dart:html' as html;
+import 'dart:async';
+import 'package:dart.html.location/location.dart' as loc;
+import 'package:dart.httprequest/request.dart' as req;
+import 'package:dart.httprequest/request_ver_html.dart' as req;
+
+//String configBackendAddr = "";
+String GetBackAddr() {
+  //return (new loc.Location()).baseAddr;
+  return "http://localhost:8080";
+}
+
+LoginNBox GetLoginNBox() {
+  return new LoginNBox();
+}
 
 void main() {
   print("hello client");
   ContentBuilder c = new ContentBuilder()..addItem("ME", "#/Me")..addItem("Home", "#/Home");
   c.bake(html.document.body);
+  //
+  //
+  loc.PageManager pageManager = new loc.PageManager();
+  pageManager.pages.add(new MePage());
+  pageManager.doLocation();
+
+  var l = new loc.Location();
+  print(">>>>> ${l.hash} : ${l.scheme} : ${l.baseAddr} : ${l.values} : ${l.baseAddr}");
 }
 
-class Config {
-
+class Config {}
+class LoginNBox {
+  String callbackopt = "cb";
+  String makeLoginTwitterUrl() {
+    var l = new loc.Location();
+    return """${GetBackAddr()}/api/v1/twitter/tokenurl/redirect?${callbackopt}=${Uri.encodeComponent(l.baseAddr+"/twitter")}"""; //"${l.baseAddr}"+"/"
+  }
 }
 
-class Page {
 
+class MePage extends loc.Page {
+  String rootID;
+  bool isExclusive;
+
+  MePage({this.rootID: "fire-mepage", this.isExclusive: true}) {}
+
+  bool updateLocation(loc.PageManager manager, loc.Location location) {
+    if (location.hash.startsWith("#/Me")) {
+      manager.doEvent(loc.PageManagerEvent.startLoading);
+      drawHtml()
+          .then((v) {
+            manager.doEvent(loc.PageManagerEvent.stopLoading);
+          })
+          .catchError((e) {})
+          .whenComplete(() {
+            manager.doEvent(loc.PageManagerEvent.stopLoading);
+          });
+    }
+    return true;
+  }
+
+  bool updateEvent(loc.PageManager manager, loc.PageManagerEvent event) {
+    return true;
+  }
+
+  Future drawHtml() async {
+    var rootElm = html.document.body.querySelector("#${rootID}");
+    rootElm.style.display = "block";
+    rootElm.children.clear();
+    rootElm.appendHtml([
+      """<a class="fire-mepage-login-item" href="${GetLoginNBox().makeLoginTwitterUrl()}"> twitter login </a>""",
+    ].join(), treeSanitizer: html.NodeTreeSanitizer.trusted);
+  }
 }
-/*
-class Location {
-  String hash = "";
 
-//  factory Location() {
-//    return new Location.url(html.window.location.href);
-//  }
-
-  Location.url(String url) {
-
-  }
-
-  static String address(String hash) {
-    if (hash == null) {
-      return "";
-    }
-    int index = hash.indexOf("?");
-    if (index < 0) {
-      return hash;
-    }
-    return hash.substring(0, index);
-  }
-
-  static Map<String, String> prop(String hash) {
-    if (hash == null) {
-      return {};
-    }
-    Map<String, String> prop = {};
-    if (hash.indexOf("?") > 0) {
-      prop = Uri.splitQueryString(hash.substring(hash.indexOf("?") + 1));
-    }
-    return prop;
-  }
-
-  static String getFAddr() {
-    return html.window.location.href.replaceFirst(new RegExp(r"""[#\?].*"""), "");
-  }
-}*/
-
-class ContentBuilder {
+class ContentBuilder extends loc.Page {
   String navigatorId = "fire-navigation";
   String navigatorRightId = "fire-navigation-right";
   String navigatorLeftId = "fire-navigation-left";
@@ -62,7 +83,7 @@ class ContentBuilder {
 
   List<String> tabList = [];
   List<String> urlList = [];
-  Map<String, html.Element> elm = {};
+  Map<String, html.Element> elms = {};
 
   ContentBuilder() {
     html.window.onHashChange.listen((html.Event ev) {
@@ -75,13 +96,28 @@ class ContentBuilder {
     urlList.add(url);
   }
 
+  bool updateEvent(loc.PageManager manager, loc.PageManagerEvent event) {
+    if(event == loc.PageManagerEvent.startLoading) {
+      for(var key in elms.keys) {
+        elms[key].style.display = "none";
+      }
+    } else if(event == loc.PageManagerEvent.stopLoading) {
+      for(var key in elms.keys) {
+        elms[key].style.display = "block";
+      }
+    }
+    return true;
+  }
+
   void onHashChange() {
     //::selectio
     //  var e = elm[html.window.location.hash];
   }
 
-  bake(html.Element rootElm) {
-    rootElm.appendHtml(["""<div id=${navigatorId} class="${navigatorId}"> </div>""", """<div id=${contentId} class="${contentId}"> </div>""", """<div id=${footerId} class="${footerId}"> </div>""",].join("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
+  bake(html.Element rootElm,{needMakeRoot:false}) {
+    if(needMakeRoot) {
+      rootElm.appendHtml(["""<div id=${navigatorId} class="${navigatorId}"> </div>""", """<div id=${contentId} class="${contentId}"> </div>""", """<div id=${footerId} class="${footerId}"> </div>""",].join("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
+    }
     //
     var navigator = rootElm.querySelector("#${navigatorId}");
     navigator.appendHtml(
@@ -97,13 +133,13 @@ class ContentBuilder {
 
     //
     var navigatorLeft = rootElm.querySelector("#${navigatorLeftId}");
-    elm.clear();
+    elms.clear();
     for (int i = 0; i < tabList.length; i++) {
       var item = new html.Element.html("""<a href="${urlList[i]}" id="${navigatorItemId}" class="${navigatorItemId}"> ${tabList[i]} </a>""", treeSanitizer: html.NodeTreeSanitizer.trusted);
       navigatorLeft.children.add(item);
-      elm[urlList[i]] = item;
+      elms[urlList[i]] = item;
       item.onClick.listen((e) {
-        for (var ee in elm.values) {
+        for (var ee in elms.values) {
           ee.classes.clear();
           if (item != ee) {
             ee.classes.add("${navigatorItemId}");
