fecff4fb479e854c8b1eb15afb5c4e54f6524c2e
kyorohiro <kyorohiro@gmail.com>
2016-10-28 11:11:39 +0900

    ..

--- web/page/art_setting.dart
+++ web/page/art_setting.dart
@@ -19,16 +19,24 @@ class ArtSettingPage extends loc.Page {
   }
 
   update(loc.Location location) async {
-    print("==>d loc :"+location.hash);
+    print("==>d loc :" + location.hash);
     var rootElm = html.document.body.querySelector("#${rootID}");
     rootElm.style.display = "block";
     rootElm.children.clear();
-    ArtInfoProp prop = await GetArtNBox().getArtFromArticleId(//
-      location.getValueAsString("articleId", ""),
-      location.getValueAsString("sign", ""),
-      mode:ArtNBox.ModeQuery
-    );
-    var artParts = new ArticleParts(prop);
-    artParts.appendUserInfoTo(rootElm, Cookie.instance);
+    ArtInfoProp prop = await GetArtNBox().getArtFromArticleId(
+        //
+        location.getValueAsString("articleId", ""),
+        location.getValueAsString("sign", ""),
+        mode: ArtNBox.ModeQuery);
+    rootElm.appendHtml([
+      """<div style="color:black;">Title</div>""",
+      """<input value="${prop.title}" style="width:100%;">""", //
+      """<div style="color:black;">Info</div>""",
+      """<textarea value="${prop.cont}" style="width:100%;height:250px;"></textarea>""", //
+      """<div style="color:black;">Tag</div>""",
+      """<input value="${prop.tag}" style="width:100%;">""", //
+      """<button>Update</button>""", //      
+      """"""
+    ].join("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
   }
 }
